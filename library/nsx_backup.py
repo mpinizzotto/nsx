97d6de131d33dffd3e98b2300bae514c0310eb4e
Matthew Pinizzotto <opusdefunk@hotmail.com>
2018-08-09 10:52:40 -0500

    initial commit

--- library/nsx_backup.py
+++ library/nsx_backup.py
@@ -1,104 +1,200 @@
+
 #!/usr/bin/env python
 #coding=utf-8
 
 
-def check_ftp(current_config, module):
-
-    current_ftp_cfg = current_config['backupRestoreSettings']['ftpSettings']
-
-    if current_ftp_cfg.get('userName') == module.params['name']:
-        return False, current_config
+def get_ftp_config(session):
+    if session.read('applianceMgrBackupSettings')['body'] is not None:
+        response = session.read('applianceMgrBackupSettings')
+        response = response['body']['backupRestoreSettings']['ftpSettings']
+        return response
     else:
-        current_config['backupRestoreSettings']['ftpSettings']['userName'] = module.params['name']
-        return True, current_config
+        return None
+		
+def update_ftp_config(session, module):
 
-    if current_ftp_cfg.get('transferProtocol') == module.params['transfer_protocol']:
-        return False, current_config
+    ftp_create_body = {'ftpSettings': 
+	                      {'userName': module.params['name'], 
+						   'passiveMode': 'true', 
+						   'useEPSV': 'true', 
+						   'password': module.params['password'], 
+						   'passPhrase': module.params['pass_phrase'], 
+						   'transferProtocol': module.params['transfer_protocol'], 
+						   'hostNameIPAddress': module.params['ip_addr'], 
+						   'backupDirectory': module.params['backup_directory'], 
+						   'filenamePrefix': module.params['file_name_prefix'], 
+						   'useEPRT': 'false', 
+						   'port': module.params['port']}
+                      }
+    return session.update('applianceMgrBackupSettingsFtp', request_body_dict=ftp_create_body)['status']
+
+
+def check_ftp(ftp_config, module):
+    
+    #changed = False
+    #if ftp_config is not None:
+    if ftp_config['userName'] == module.params['name']:
+        changed = False
+        return changed
     else:
-        current_config['backupRestoreSettings']['ftpSettings']['transferProtocol'] = module.params['transfer_protocol']
-        return True, current_config
-
-    if current_ftp_cfg.get('hostNameIPAddress') == module.params['ip_addr']:
-        return False, current_config
+        changed = True
+        return changed
+    
+    if ftp_config['transferProtocol'] == module.params['transfer_protocol']:
+        changed = False
+        return changed
     else:
-        current_config['backupRestoreSettings']['ftpSettings']['hostNameIPAddress'] = module.params['ip_addr']
-        return True, current_config
-
-    if current_ftp_cfg.get('backupDirectory') == module.params['backup_directory']:
-        return False, current_config
+        changed = True
+        return changed
+    
+    if ftp_config['hostNameIPAddress'] == module.params['ip_addr']:
+        changed = False
+        return changed
     else:
-        current_config['backupRestoreSettings']['ftpSettings']['backupDirectory'] = module.params['backup_directory']
-        return True, current_config
-
-    if current_ftp_cfg.get('filenamePrefix') == module.params['file_name_prefix']:
-        return False, current_config
+        changed = True
+        return changed
+    
+    if ftp_config['backupDirectory'] == module.params['backup_directory']:
+        changed = False
+        return changed
+    else: 
+        changed = True
+        return changed
+    
+    if ftp_config['filenamePrefix'] == module.params['file_name_prefix']:
+        changed = False
+        return changed
     else:
-        current_config['backupRestoreSettings']['ftpSettings']['filenamePrefix'] = module.params['file_name_prefix']
-        return True, current_config
+        changed = True
+        return changed
+    
+    if ftp_config['password'] == module.params['password']:
+        changed = False
+        return changed
+    else:
+        changed = True
+        return changed
+	
+    if ftp_config['passPhrase'] == module.params['pass_phrase']:
+        changed = False
+        return changed
+    else:
+        changed = True
+        return changed
 
-    if module.params['password']:
-        current_config['backupRestoreSettings']['ftpSettings']['password'] = module.params['password']
-        return True, current_config
-
-    if module.params['pass_phrase']:
-    current_config['backupRestoreSettings']['ftpSettings']['passPhrase'] = module.params['pass_phrase']
-        return True, current_config
+    #else:
+    #    changed = True
+    #   return changed
 
 
-def get_current_config(client_session):
-    response = client_session.read('applianceMgrBackupSettings')
-    return response['body']
+def delete_config(session):
+    return session.delete('applianceMgrBackupSettings')['status']
 
-
-def update_config(client_session, current_config):
-    client_session.update('applianceMgrBackupSettings',
-                          request_body_dict=current_config)
-
-def reset_config(client_session):
-    client_session.delete('applianceMgrBackupSettings')
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
             state=dict(default='present', choices=['present', 'absent']),
             nsxmanager_spec=dict(required=True, no_log=True, type='dict'),
-            name=dict(reguired=True, type='str'),
-            password=dict(reguired=True, type='str')
-            transfer_protocol=dict(reguired=True, default='FTP', choices=['FTP', 'SFTP']),
-            ip_addr=dict(reguired=True, type='str'),
-            backup_directory=dict(reguired=True, type='str'),
+            name=dict(required=True, type='str'),
+            password=dict(required=True, no_log=True, type='str'),
+            transfer_protocol=dict(default='FTP', choices=['FTP', 'SFTP']),
+            ip_addr=dict(required=True, type='str'),
+            port=dict(default='21', type='str'),
+            backup_directory=dict(required=True, type='str'),
             file_name_prefix=dict(type='str'),
-            pass_phrase=dict(required=True, type='str')
-            #backup_frequency=dict(required=True, type='dict'),
+            pass_phrase=dict(required=True, no_log=True, type='str')
+            #backup_frequency=dict(required=True, type='dict'),            
         ),
         supports_check_mode=False
     )
 
     from nsxramlclient.client import NsxClient
 
-    client_session = NsxClient(module.params['nsxmanager_spec']['raml_file'], module.params['nsxmanager_spec']['host'],
-                               module.params['nsxmanager_spec']['user'], module.params['nsxmanager_spec']['password'])
+    session = NsxClient(module.params['nsxmanager_spec']['raml_file'], module.params['nsxmanager_spec']['host'],
+                  module.params['nsxmanager_spec']['user'], module.params['nsxmanager_spec']['password'])
 
 
-    current_config = get_current_config(client_session)
+    ftp_changed = False
+    ftp_config = get_ftp_config(session)
+    
+    if module.params['state'] == 'absent' and ftp_config:
+        delete_config(session)
+        module.exit_json(changed=True, ftp_config=None)
+   
+    if module.params['state'] == 'absent' and not ftp_config:
+        module.exit_json(changed=False, ftp_config=None)
 
-    if current_config and module.params['state'] == 'absent':
-        reset_config(client_session)
-        module.exit_json(changed=True, current_config=None)
+    if module.params['state'] == 'present' and not ftp_config:
+        update_ftp_config(session, module)
+        module.exit_json(changed=True, ftp_config=ftp_config)
+        ftp_changed = True
 
-    if not current_config and module.params['state'] == 'absent':
-        module.exit_json(changed=False, current_config=None)
-
-    changed_ftp, current_config = check_ftp(current_config, module)
-
-    if (changed_ftp):
-        update_config(client_session, current_config)
-        module.exit_json(changed=True, current_config=current_config)
+    changed_ftp = check_ftp(ftp_config, module)
+    if (changed_ftp): 
+        delete_config(session)
+        update_ftp_config(session, module)
+        module.exit_json(changed=True, ftp_config=ftp_config)
     else:
-        module.exit_json(changed=False, current_config=current_config)
+        module.exit_json(changed=False, ftp_config=None)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+    #if ftp_config and module.params['state'] == 'absent':
+    #    delete_config(session)
+    #    module.exit_json(changed=True, ftp_config=None)
+    #
+    #if not ftp_config and module.params['state'] == 'absent':
+    #    module.exit_json(changed=False, ftp_config=None)
+	#	
+	#if ftp_config and module.params['state'] == 'present':
+    #    changed_ftp = check_ftp(ftp_config, module) 
+    #    if changed_ftp:
+    #        update_ftp_config(session, module)
+    #        module.exit_json(changed=True, ftp_config='1')
+    #        ftp_changed = True
+    #
+    #if not ftp_config and module.params['state'] == 'present':   
+    #    update_ftp_config(session, module)
+    #    ftp_changed = True
+    #
+    #
+    #
+    #
+    #if ftp_changed:
+    #    module.exit_json(changed=True, ftp_config=ftp_config)
+    #else:
+    #    module.exit_json(changed=False, ftp_config='2')
+    #
 
 
 from ansible.module_utils.basic import *
-
 if __name__ == '__main__':
     main()
+
